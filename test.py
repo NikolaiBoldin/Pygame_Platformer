b1227fb1d8198a65c17e17bf718654701e32ad93
Home PC <nikolajboldin1@yandex.ru>
2020-01-14 15:43:14 +0300

    Изменен прыжок и добавлен класс врагов

--- test.py
+++ test.py
@@ -5,25 +5,25 @@ import data.maps.tmx as tmx
 from pygame import *
 
 
-# class Enemy(pygame.sprite.Sprite):
-#     image = pygame.image.load('enemy.png')
-#
-#     def __init__(self, location, *groups):
-#         super(Enemy, self).__init__(*groups)
-#         self.rect = pygame.rect.Rect(location, self.image.get_size())
-#         self.direction = 1
-#
-#     def update(self, dt, game):
-#         self.rect.x += self.direction * 100 * dt
-#         for cell in game.tile_map.layers['triggers'].collide(self.rect, 'reverse'):
-#             if self.direction > 0:
-#                 self.rect.right = cell.left
-#             else:
-#                 self.rect.left = cell.right
-#             self.direction *= -1
-#             break
-#         if self.rect.colliderect(game.player.rect):
-#             game.player.is_dead = True
+class Enemy(pygame.sprite.Sprite):
+    image = pygame.image.load(os.path.join('data/sprites/', 'Skeleton.png'))
+
+    def __init__(self, location, *groups):
+        super(Enemy, self).__init__(*groups)
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
+        self.direction = 1
+
+    def update(self, dt, game):
+        self.rect.x += self.direction * 100 * dt
+        for cell in game.tile_map.layers['Triggers'].collide(self.rect, 'reverse'):
+            if self.direction > 0:
+                self.rect.right = cell.left
+            else:
+                self.rect.left = cell.right
+            self.direction *= -1
+            break
+        if self.rect.colliderect(game.player.rect):
+            game.player.is_dead = True
 
 
 # class Bullet(pygame.sprite.Sprite):
@@ -57,11 +57,19 @@ class Player(pygame.sprite.Sprite):
         x, y = self.image.get_size()
         self.rect = pygame.rect.Rect(location, self.image.get_size())
         self.masc = pygame.rect.Rect((X + 23, Y + 15), (x - 46, y - 15))
-        self.resting = False
-        self.dy = 0
+        self.on_the_ground = False  # находится ли персонаж на земле
         self.is_dead = False
-        self.direction = 1
+        self.direction = 1  # направление персонажа: 1 -> право, -1 -> лево
         self.gun_cooldown = 0
+
+        self.jumpforce = 0  # сила прыжка
+        self.gravityforce = 0  # сила гравитации
+        self.maxgravityforce = 50  # сила максимальная сила гравитации
+
+    def gravity(self):
+        # Событие начала прыжка игрока
+        self.jumpforce = 15
+        self.gravityforce = 0
 
     def update(self, dt, game):
 
@@ -70,17 +78,17 @@ class Player(pygame.sprite.Sprite):
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            # self.rect.x -= 300 * dt
-            # self.masc.x -= 300 * dt
-            self.rect.x -= 5
-            self.masc.x -= 5
+            self.rect.x -= 300 * dt
+            self.masc.x -= 300 * dt
+            # self.rect.x -= 15
+            # self.masc.x -= 15
             self.image = self.left_image
             self.direction = -1
         if key[pygame.K_RIGHT]:
-            # self.rect.x += 300 * dt
-            # self.masc.x += 300 * dt
-            self.rect.x += 5
-            self.masc.x += 5
+            self.rect.x += 300 * dt
+            self.masc.x += 300 * dt
+            # self.rect.x +=15
+            # self.masc.x += 15
             self.image = self.right_image
             self.direction = 1
 
@@ -93,33 +101,40 @@ class Player(pygame.sprite.Sprite):
 
         self.gun_cooldown = max(0, self.gun_cooldown - dt)
 
-        if self.resting and key[pygame.K_SPACE]:
-            self.dy = -500
-        self.dy = min(400, self.dy + 40)
-
-        self.rect.y += self.dy * dt
-        self.masc.y += self.dy * dt
+        if self.on_the_ground:
+            if key[pygame.K_SPACE]:  # прыжок
+                self.gravity()
+                self.on_the_ground = False
+        if not self.on_the_ground:
+            if self.gravityforce >= self.maxgravityforce:
+                self.gravityforce = self.maxgravityforce
+            else:
+                self.gravityforce += 1
+            self.rect.y -= self.jumpforce
+            self.rect.y += self.gravityforce
+            self.masc.y -= self.jumpforce
+            self.masc.y += self.gravityforce
 
         new = self.rect
         new_masc = self.masc
-        self.resting = False
+        self.on_the_ground = False
         for cell in game.tile_map.layers['Blocking'].collide(new_masc, 'blockers'):
             blockers = cell['blockers']
             if 'l' in blockers and last_masc.right <= cell.left < new_masc.right:
-                new.right = cell.left+23
+                new.right = cell.left + 23
                 new_masc.right = cell.left
             if 'r' in blockers and last_masc.left >= cell.right > new_masc.left:
-                new.left = cell.right-23
+                new.left = cell.right - 23
                 new_masc.left = cell.right
             if 't' in blockers and last_masc.bottom <= cell.top < new_masc.bottom:
-                self.resting = True
+                self.jumpforce = 0
+                self.gravityforce = 0
+                self.on_the_ground = True
                 new.bottom = cell.top
                 new_masc.bottom = cell.top
-                self.dy = 0
             if 'b' in blockers and last_masc.top >= cell.bottom > new_masc.top:
-                new.top = cell.bottom-15
+                new.top = cell.bottom - 15
                 new_masc.top = cell.bottom
-                self.dy = 0
 
         game.tile_map.set_focus(new.x, new.y)
 
@@ -144,21 +159,20 @@ class Game:
         self.player = Player((start_cell.px, start_cell.py), self.sprites)
         self.tile_map.layers.append(self.sprites)
 
-        # for enemy in self.tile_map.layers['triggers'].find('enemy'):
-        #     Enemy((enemy.px, enemy.py), self.enemies)
-        # self.tile_map.layers.append(self.enemies)
+        for enemy in self.tile_map.layers['Triggers'].find('enemy'):
+            Enemy((enemy.px, enemy.py), self.enemies)
+        self.tile_map.layers.append(self.enemies)
 
         while 1:
             dt = clock.tick(self.fps)
-            # print(dt)
-            # clock.tick(self.fps)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     return
 
-            self.tile_map.update(dt / 1000., self)
+            self.tile_map.update(dt / 1000, self)
             # screen.blit(background, (0, 0))
             screen.fill(pygame.Color("black"))
             self.tile_map.draw(screen)
