6944e661254a49db4d78b0af95f565bd352843c7
ppelageya <vppolina2230@mail.ru>
2020-01-17 13:28:19 +0300

    Анимация второго экрана

--- main.py
+++ main.py
@@ -46,11 +46,9 @@ class Game:
         self.timer_of_update_MainMenu = 0  # таймер обновлений
         self.direction_MainMenu = 1
 
-        self.flag = True
-        self.start_game = True
+        self.clicked_start_game = True
+        self.clicked_go_over_game = True
 
-        self.flag = True
-        self.start_game = True
 
     def main(self, screen):
         clock = time.Clock()
@@ -110,24 +108,24 @@ class Game:
             display.update()
 
     def action1(self):
-        self.flag = False
+        self.clicked_start_game = False
 
     def action2(self):
-        self.start_game = False
+        self.clicked_go_over_game = False
 
-    def button_control(self, x, y, width_b, height_b, screen):
-        mouse = pygame.mouse.get_pos()
-        click_mouse = pygame.mouse.get_pressed()
-
-        if x < mouse[0] < x + width_b and y < mouse[1] < y + height_b:
-            pygame.draw.rect(screen, (95, 158, 160), (x, y, width_b, height_b))
-            if click_mouse[0] == 1:
-                action()
-        else:
-            pygame.draw.rect(screen, (47, 79, 79), (x, y, width_b, height_b))
-        font_b = pygame.font.Font('17810.ttf', 30)
-        text = font_b.render(u'Управление', True, (176, 224, 230))
-        screen.blit(text, (x + 10, y + 10))
+    # def button_control(self, x, y, width_b, height_b, screen):
+    #     mouse = pygame.mouse.get_pos()
+    #     click_mouse = pygame.mouse.get_pressed()
+    #
+    #     if x < mouse[0] < x + width_b and y < mouse[1] < y + height_b:
+    #         pygame.draw.rect(screen, (95, 158, 160), (x, y, width_b, height_b))
+    #         if click_mouse[0] == 1:
+    #             action()
+    #     else:
+    #         pygame.draw.rect(screen, (47, 79, 79), (x, y, width_b, height_b))
+    #     font_b = pygame.font.Font('data/fonts/17810.ttf', 30)
+    #     text = font_b.render(u'Управление', True, (176, 224, 230))
+    #     screen.blit(text, (x + 10, y + 10))
 
     def button_game(self, x, y, width_b, height_b, screen):
         mouse = pygame.mouse.get_pos()
@@ -140,46 +138,64 @@ class Game:
                 self.main(screen)
         else:
             pygame.draw.rect(screen, (47, 79, 79), (x, y, width_b, height_b))
-        font_b = pygame.font.Font('17810.ttf', 26)
+        font_b = pygame.font.Font('data/fonts/17810.ttf', 26)
         text = font_b.render(u'Перейти к игре', True, (176, 224, 230))
         screen.blit(text, (x + 10, y + 10))
 
     def start_screen2(self, screen):
-        fon = pygame.transform.scale(load_image('fon_dark.jpg'), (900, 600))
-        screen.blit(fon, (0, 0))
-        pygame.font.init()
-        myfont = pygame.font.Font('17810.ttf', 60)
-        string_rendered = myfont.render(u'Witch adventure', 1, (47, 79, 79))
-        screen.blit(string_rendered, (220, 50))
-        intro_text = ["", "", "", "", ""
-                                      "В нашей игре ведьма Моргана сталкивается",
-                      " с разными испытаниями. На каждом уровне ",
-                      "её ждет новое задание. Чтобы получить метлу, ",
-                      "Ключ от тайной комнаты и суперспособности, ",
-                      "Моргане придется бороться с монстрами и идти ",
-                      "в нужном направлении. Но Моргана не сдается ",
-                      "несмотря на все трудности."]
-        newfont = pygame.font.Font('17810.ttf', 20)
-        text_coord = 50
-        for line in intro_text:
-            string_rendered = newfont.render(line, 1, (176, 224, 230))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 180
-            text_coord += intro_rect.height
-            screen.blit(string_rendered, intro_rect)
+        clock = time.Clock()
+        screen.blit(self.fons[0], (0, 0))
+        font.init()
+        my_font = font.Font('data/fonts/17810.ttf', 60)
+        string_rendered = my_font.render(u'Witch adventure', 1, (47, 79, 79))
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.terminate()
-                elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
-                    self.pause()
+            dt = clock.tick(60) / 1000
+            clock.tick(self.fps)
+            self.timer_of_update_MainMenu += dt
+            for ev in event.get():
+                if ev.type == QUIT:
+                    return
+                if ev.type == KEYDOWN and ev.key == K_ESCAPE:
+                    return
 
-            self.button_control(130, 500, 210, 50, screen)
+            if self.timer_of_update_MainMenu >= self.update_rate_MainMenu:
+                screen.fill(Color("black"))
+                if self.direction_MainMenu == 1:
+                    screen.blit(self.fons[self.frame_nomber_MAIN_MENU], (0, 0))
+                else:
+                    if self.frame_nomber_MAIN_MENU == 0:
+                        self.frame_nomber_MAIN_MENU += 1
+                    screen.blit(self.fons[-1 - self.frame_nomber_MAIN_MENU], (0, 0))
+                self.frame_nomber_MAIN_MENU = (self.frame_nomber_MAIN_MENU + 1) % len(self.fons)
+                if self.frame_nomber_MAIN_MENU == 0:
+                    self.direction_MainMenu *= -1
+                self.timer_of_update_MainMenu = 0
+                s = pygame.Surface((900, 600), pygame.SRCALPHA)  # per-pixel alpha
+                s.fill((0, 0, 0, 128))  # notice the alpha value in the color
+                screen.blit(s, (0, 0))
+            intro_text = ["", "",
+                          "В нашей игре ведьма Моргана сталкивается",
+                          " с разными испытаниями. На каждом уровне ",
+                          "её ждет новое задание. Чтобы получить метлу, ",
+                          "Ключ от тайной комнаты и суперспособности, ",
+                          "Моргане придется бороться с монстрами и идти ",
+                          "в нужном направлении. Но Моргана не сдается ",
+                          "несмотря на все трудности."]
+            newfont = pygame.font.Font('data/fonts/17810.ttf', 20)
+            text_coord = 80
+            for line in intro_text:
+                string_rendered_text = newfont.render(line, 1, (176, 224, 230))
+                intro_rect = string_rendered_text.get_rect()
+                text_coord += 10
+                intro_rect.top = text_coord
+                intro_rect.x = 180
+                text_coord += intro_rect.height
+                screen.blit(string_rendered_text, intro_rect)
+            # self.button_control(130, 500, 210, 50, screen)
             self.button_game(520, 500, 230, 50, screen)
-            if self.start_game is False:
+            screen.blit(string_rendered, (200, 50))
+            if self.clicked_go_over_game is False:
                 break
             pygame.display.flip()
             clock.tick(self.fps)
@@ -233,7 +249,7 @@ class Game:
 
             self.button(320, 280, 240, 55, screen)
             screen.blit(string_rendered, (200, 50))
-            if self.flag is False:
+            if self.clicked_start_game is False:
                 break
             display.flip()
             display.update()
